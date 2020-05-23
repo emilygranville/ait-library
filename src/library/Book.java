1023f2a9bcacc2a09ffa0b9f49f750f20b29b720
Emily <efg12103@gmail.com>
2020-05-23 14:00:06 -0400

    book class p2

---
+++ src/library/Book.java
@@ -0,0 +1,43 @@
+package library;
+
+public class Book {
+	private int id;
+	private String title;
+	private String author;
+	private int copies;
+	private int available;
+	
+	public Book(int id, String title, String author, int copies, int available) {
+		super();
+		
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.copies = copies;
+		this.available = available;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public int getCopies() {
+		return copies;
+	}
+	
+	public void setCopies(int copies) {
+		this.copies = copies;
+	}
+	
+	public int getAvailable() {
+		return available;
+	}
+	
+	public void setAvailable() {
+		this.available = available;
+	}
+}
